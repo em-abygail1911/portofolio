54624cae52cc49119db1a74aa6aac492655f2c84
em-abygail1911 <135339063+em-abygail1911@users.noreply.github.com>
2023-10-13 16:48:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,122 @@
-function calculateLuas() {
-  // Get the user's input
-  const sideLength = parseFloat(document.getElementById('sideLength').value);
+let blogs = [];
 
-  // Check if the input is a valid number
-  if (isNaN(sideLength)) {
-      alert('Please enter a valid number for the side length.');
-      return;
+function saveBlog() 
+{
+  const titleInput = document.getElementById('blogTitle');
+  const contentInput = document.getElementById('blogContent');
+
+  const title = titleInput.value;
+  const content = contentInput.value;
+
+  if (title && content) {
+    const blog = { title, content };
+
+    const existingBlogIndex = blogs.findIndex(b => b.title === title);
+    if (existingBlogIndex !== -1) {
+      blogs[existingBlogIndex] = blog;
+    } else {
+      blogs.push(blog);
+    }
+
+    titleInput.value = '';
+    contentInput.value = '';
+
+    renderBlogPreviews();
+    hideEditor();
   }
-
-  // Calculate the area of the square
-  const luas = sideLength * sideLength;
-  const keliling = 4 * sideLength;
-
-  // Display the result
-  document.getElementById('result_luas').innerHTML = `Luas persegi ini adalah ${luas}`;
-}
-function calculateKeliling() {
-  // Get the user's input
-  const sideLength = parseFloat(document.getElementById('sideLength').value);
-
-  // Check if the input is a valid number
-  if (isNaN(sideLength)) {
-      alert('Please enter a valid number for the side length.');
-      return;
-  }
-
-  // Calculate the area of the square
-  const luas = sideLength * sideLength;
-  const keliling = 4 * sideLength;
-
-  // Display the result
-  document.getElementById('result_keliling').innerHTML = `The area of the square is: ${keliling}`;
 }
 
-// Attach the calculateArea function to the button click event
-document.getElementById('calculateLuas').addEventListener('click', calculateLuas);
-document.getElementById('calculateKeliling').addEventListener('click', calculateKeliling);
+function renderBlogPreviews() 
+{
+  const blogList = document.getElementById('blogList');
+  blogList.innerHTML = '';
+
+  blogs.forEach((blog, index) => {
+    const blogPreview = document.createElement('div');
+    blogPreview.classList.add('blog-preview');
+
+    const title = document.createElement('h3');
+    title.textContent = blog.title;
+
+    const previewContent = document.createElement('p');
+    const content = blog.content.length > 50 ? blog.content.substring(0, 50) + '...' : blog.content;
+    previewContent.textContent = content;
+
+    const viewButton = document.createElement('button');
+    viewButton.textContent = 'View';
+    viewButton.addEventListener('click', () => showBlog(index));
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.addEventListener('click', () => editBlog(index));
+
+    blogPreview.appendChild(title);
+    blogPreview.appendChild(previewContent);
+    blogPreview.appendChild(viewButton);
+    blogPreview.appendChild(editButton);
+
+    blogList.appendChild(blogPreview);
+  });
+}
+
+function showBlog(index) 
+{
+  const blog = blogs[index];
+  const blogList = document.getElementById('blogList');
+
+  blogList.innerHTML = '';
+
+  const backButton = document.createElement('button');
+  backButton.textContent = 'Back';
+  backButton.addEventListener('click', renderBlogPreviews);
+
+  const title = document.createElement('h2');
+  title.textContent = blog.title;
+
+  const content = document.createElement('p');
+  content.textContent = blog.content;
+
+  blogList.appendChild(backButton);
+  blogList.appendChild(title);
+  blogList.appendChild(content);
+}
+
+function editBlog(index) 
+{
+  const blog = blogs[index];
+  const titleInput = document.getElementById('blogTitle');
+  const contentInput = document.getElementById('blogContent');
+  const saveButton = document.getElementById('saveButton');
+
+  titleInput.value = blog.title;
+  contentInput.value = blog.content;
+  saveButton.textContent = 'Update';
+
+  showEditor();
+}
+
+function showEditor() 
+{
+  const editor = document.getElementById('editor');
+  const addButton = document.getElementById('addButton');
+
+  editor.style.display = 'block';
+  addButton.style.display = 'none';
+}
+
+function hideEditor() 
+{
+  const editor = document.getElementById('editor');
+  const addButton = document.getElementById('addButton');
+  const titleInput = document.getElementById('blogTitle');
+  const contentInput = document.getElementById('blogContent');
+  const saveButton = document.getElementById('saveButton');
+
+  editor.style.display = 'none';
+  addButton.style.display = 'block';
+  titleInput.value = '';
+  contentInput.value = '';
+  saveButton.textContent = 'Save';
+}
+
+renderBlogPreviews();
